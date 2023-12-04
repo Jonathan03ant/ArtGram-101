82f6bfbede808fc730f573d0f7bfe434d8db335b
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-04 17:39:25 -0500

    client side js updated to handle creating account

---
+++ public/client.js
@@ -0,0 +1,30 @@
+/*
+    *part1, sending post request to create an account
+*/
+
+const create = document.querySelector("#accountCreationForm");
+create.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    const data = `firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}
+                    &username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/createaccount");
+    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhttp.onload = function(){
+        if (xhttp.status === 200){
+            console.log(xhttp.responseText);
+        } else {
+            console.error("Error:", xhttp.statusText);
+        }
+    };
+
+    xhttp.send(data);
+});
