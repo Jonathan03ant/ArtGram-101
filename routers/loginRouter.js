fa85ce1d1813e08dbf9fdc431a0f82545aaf930e
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-05 17:36:18 -0500

    Login router update

---
+++ routers/loginRouter.js
@@ -0,0 +1,31 @@
+const express = require ('express')
+const router = express.Router();
+const User = require('../models/usersModel')
+
+router.get('/login', (req, res, next) =>{
+    res.render("login")
+});
+
+router.post('/login', async (req, res, next) =>{
+    const { username, password } = req.body;
+    /*
+        *Find the user name from the Database
+        *If it is not found, send error message
+    */
+    const user = await User.findOne({username: username});
+    if (!user){
+        return res.status(400).json({ message: "User does not exist"});
+    }
+    if (user.password !== password) {
+        return res.status(400).json({ message: "Incorrect password"});
+    }
+
+    req.session.user = user;
+    /*
+        *This is being sent back to client.js
+    */
+    res.json({ message: "success", username: username, accountType: user.accountType});
+
+})
+
+module.exports = router;
