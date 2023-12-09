5cddae1b716f931a5ea2c74b9a091aa530ffe415
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-08 23:57:15 -0500

    Artwork and users model updated to accout for Artists model

--- models/artworkModel.js
+++ models/artworkModel.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+/*
+    *Artworks have the pre-defined keys and
+    *An array of likes-->referencing to users
+    *An array of reviews where
+        *A review is associated with a user
+        *And a string of review
+*/
 const artworkSchema = new Schema ({
     title: {type: String, required: true},
-    artist: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    artist: {type: mongoose.Schema.Types.ObjectId, ref: 'Artist', required: true},
     year: {type: String, required: true},
     category: {type: String, required: true},
     medium: {type: String, required: true},
     description: {type: String, required: true},
     poster: {type: String, required: true},
+    likes: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    reviews: [{
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        review: String
+    }]
 });
 
 const Artwork = mongoose.model('Artwork', artworkSchema);
--- models/usersModel.js
+++ models/usersModel.js
@@ -14,6 +14,7 @@ const userSchema = new Schema({
         enum: ['patron', 'artist']},
     firstName: String,
     lastName: String,
+    following: [{type: mongoose.Schema.Types.ObjectId, ref: 'Artist'}],
 });
 
 /*
