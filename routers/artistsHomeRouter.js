7dabb60bbd49c4408dd96856d748c9e4996cc1ac
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-10 08:40:55 -0500

    router + logo

--- routers/artistsHomeRouter.js
+++ routers/artistsHomeRouter.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router();
+const Artist = require('../models/artistsModel');
 
-router.get("/artist/home", (req, res, next) => {
-    res.render('artistsHomePage', {accountType: 'artist'});
+router.get("/artist/home", async (req, res, next) => {
+    const artist = await Artist.findOne({ user: req.session.userId }).populate('Artworks').populate('workshops');
+    res.render('artistsHomePage', {accountType: 'artist', artworks: artist.Artworks, workshops: artist.workshops});
 });
 
 module.exports = router;
