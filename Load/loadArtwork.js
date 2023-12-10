b2500d2c0f77f52bf70ae0900a54d28ccad99fb9
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-10 08:37:02 -0500

    Loading artwork modified

--- Load/loadArtwork.js
+++ Load/loadArtwork.js
@@ -16,42 +16,45 @@ async function loadArtwork() {
         /*
             *Check if the artwork already exists
         */
-        const artistUser = await User.findOne({ username: artworkData.Artist.replace(/ /g, '_') });
-        const existingArtwork = await Artwork.findOne({ title: artworkData.Title, artist: artistUser._id });
-        if (existingArtwork) {
-            console.log(`Artwork already exists: ${artworkData.Title}`);
-            continue;
-        }
+        
+        try{
+            const artistUser = await User.findOne({ username: artworkData.Artist.replace(/ /g, '_') });
+            const existingArtwork = await Artwork.findOne({ title: artworkData.Title, artist: artistUser._id });
+            if (existingArtwork) {
+                console.log(`Artwork already exists: ${artworkData.Title}`);
+                continue;
+            }
 
-        /*
-            Create the new artwork
-        */
-        const artwork = new Artwork({
-            title: artworkData.Title,
-            artist: artistUser._id,
-            year: artworkData.Year,
-            category: artworkData.Category,
-            medium: artworkData.Medium,
-            description: artworkData.Description || "No description Provided",
-            poster: artworkData.Poster,
-        });
-        artwork.save()
-            .then(() =>{
-                console.log(`Artwork Created for ${artworkData.Title}`);
-                return Artist.findOne({ user: artistUser._id });
-            })
-            .then(artist => {
-                if (artist){
-                    artist.Artworks.push(artwork._id);
-                    return artist.save();
-                }
-            })
-            .then(() => console.log(`Artwork added to artist: ${artworkData.Artist}`))
-            .catch(err => console.log(`Error creating artwork for ${artworkData.Title}: ${err}`));
+            /*
+                Create the new artwork
+            */
+            const artwork = new Artwork({
+                title: artworkData.Title,
+                artist: artistUser._id,
+                artistName: artworkData.Artist,
+                year: artworkData.Year,
+                category: artworkData.Category,
+                medium: artworkData.Medium,
+                description: artworkData.Description || "No description Provided",
+                poster: artworkData.Poster,
+            });
+
+            await artwork.save();
+            console.log(`Artwork Created for ${artworkData.Title}`);
+
+            const artist = await Artist.findOne({ user: artistUser._id });
+            if (artist) {
+                artist.Artworks.push(artwork._id);
+                await artist.save();
+                console.log(`Artwork added to artist: ${artworkData.Artist}`);
+            }
+        } catch (err) {
+            console.log(`Error creating artwork for ${artworkData.Title}: ${err}`);
+        }
     }
 }
+
 console.log("Loaded Artworks!")
 loadArtwork();
 
 
-  
